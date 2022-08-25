c24d786ea05da35ed522e919baa94b5a8a826c80
Jinyoung Kim <gojillajin@gmail.com>
2022-08-26 00:31:48 +0900

    add component

---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+export default function Nav () {
+    return (
+        <ol>
+            <li><a href="">html</a></li>
+            <li><a href="">css</a></li>
+            <li><a href="">js</a></li>
+        </ol>
+    );
+}
